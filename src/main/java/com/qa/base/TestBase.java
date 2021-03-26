90fbafe5fda43aea17ddb6cdebc4e8308952767f
Urvashime <uam11970@hotmail.com>
2021-03-25 18:48:20 -0700

    Update TestBase.java

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -1,5 +1,17 @@
 package com.qa.base;
 
 public class TestBase {
+  
+  public TestBase()
+  {
+    FileInputStream fis;
+    Properties prop = new Properties();
+    try{
+      fis = new FileInputStream("/Users/urvashimehta/eclipse-workspace/TestNGSessions/src/test/java/com/qa/util/config.properties");
+      prop.load(fis);
+    }catch(Exception e)
+    {e.printStackTrace();}
+    
+  }
 
 }
